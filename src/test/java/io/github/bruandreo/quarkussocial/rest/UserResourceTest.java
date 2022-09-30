4925013192658a413e001a38bc8b17acaffb5c5a
Bruno Andreo <brunolino2026@gmail.com>
2022-09-29 22:47:51 -0300

    test: added test to create user with an error in body

--- src/test/java/io/github/bruandreo/quarkussocial/rest/UserResourceTest.java
+++ src/test/java/io/github/bruandreo/quarkussocial/rest/UserResourceTest.java
@@ -1,10 +1,14 @@
 package io.github.bruandreo.quarkussocial.rest;
 
 import io.github.bruandreo.quarkussocial.rest.dto.CreateUserRequest;
+import io.github.bruandreo.quarkussocial.rest.dto.ResponseError;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,4 +36,25 @@ class UserResourceTest {
         assertNotNull(response.jsonPath().getString("id"));
     }
 
+    @Test
+    @DisplayName("should return error when body is not valid")
+    public void createUserValidationError() {
+        var user = new CreateUserRequest();
+        user.setName(null);
+
+        var response =
+                given()
+                    .contentType(ContentType.JSON)
+                    .body(user)
+                .when()
+                    .post("/users")
+                .then()
+                    .extract().response();
+
+        assertEquals(ResponseError.UNPROCESSABLE_REQUEST_STATUS, response.statusCode());
+        assertEquals("Validation Error", response.jsonPath().getString("message"));
+
+        List<Map<String, String>> errors = response.jsonPath().getList("fieldErrors");
+        assertNotNull(errors.get(0).get("message"));
+    }
 }
